521288d7c7fff660812d2a3636c3113fd828bfb0
zhchen8 <zhchen8leqee.com>
2023-10-30 15:49:53 +0800

    [重构] 升级到2.2，增加json格式化

--- src/hertz_packet/notice/__init__.py
+++ src/hertz_packet/notice/__init__.py
@@ -8,6 +8,7 @@
 @Desc    : 发送一些消息
 """
 import json
+from json import JSONDecodeError
 
 import requests
 
@@ -26,12 +27,19 @@ def send_note(_k, _v, _memo: dict = None):
 
 
 def get_kv(_k, default=None):
+    """Since 2.2.0 增加自动转换memo"""
     headers = {'content-type': 'application/json'}
     params = {"key": _k}
     if default is not None:
         params['default'] = default
     response = requests.get(f"{note_server_url}/kv", headers=headers, params=params)
-    return response.json()['msg']
+    _msg = response.json()['msg']
+    if 'memo' in _msg and _msg['memo']:
+        try:
+            _msg['memo'] = json.loads(_msg['memo'])
+        except JSONDecodeError:
+            pass
+    return _msg
 
 
 def set_kv(_k, _v, _memo: dict = None):
