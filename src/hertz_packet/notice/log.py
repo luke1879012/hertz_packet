d44bc10875a5d0f65d94d26d5d7303bc3ddbfc26
zhchen8 <zhchen8leqee.com>
2023-11-03 10:06:50 +0800

    [bug] 修复并发问题

--- src/hertz_packet/notice/log.py
+++ src/hertz_packet/notice/log.py
@@ -9,22 +9,24 @@
 """
 import logging
 import os
+import random
 import sys
 from datetime import datetime
 
 
 class Logger:
     def __init__(self, log_path=None, file_name=None):
-        self.logger = logging.getLogger(__name__)  # 创建日志记录器
+        logger_name = file_name or f"{__name__}{random.randint(0, 15)}"
+        self.logger = logging.getLogger(logger_name)  # 创建日志记录器
         self.logger.setLevel(logging.INFO)  # 日志记录器的等级
 
         _format = logging.Formatter('%(asctime)s [%(filename)s] %(levelname)s: %(message)s',
                                     datefmt='%Y-%m-%d %H:%M:%S')
 
-        file_name_date = datetime.now().strftime("%H_%M_%S")
-        filename = file_name or f"{os.path.basename(sys.argv[0]).split('.')[0]}_{file_name_date}.log"
-        path_name_date = datetime.now().strftime("%Y_%m_%d")
-        log_path = log_path or f'./log/{path_name_date}'
+        today = datetime.now().strftime("%Y_%m_%d")
+        log_path = log_path or f'./log/{today}'
+        now = datetime.now().strftime("%H_%M_%S")
+        filename = file_name or f"{os.path.basename(sys.argv[0]).split('.')[0]}_{now}.log"
         os.path.exists(log_path) or os.makedirs(log_path)
         file_handler = logging.FileHandler(f"{log_path}/{filename}", encoding='utf-8')
         file_handler.setLevel(logging.INFO)
